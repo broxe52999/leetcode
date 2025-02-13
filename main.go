b41a14af479bb055812bf6e970be4df66217a791
xjx <1961409642@qq.com>
2025-02-13 17:21:27 +0800

    [fix] 2.13刷题两道

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+// 轮转数组
+func rotate(nums []int, k int) {
+	n := len(nums)
+	k = k % n
+	reverse(nums, 0, n-1)
+	reverse(nums, 0, k-1)
+	reverse(nums, k, n-1)
+}
+
+func reverse(nums []int, start int, end int) {
+	for start < end {
+		nums[start], nums[end] = nums[end], nums[start]
+		start++
+		end--
+	}
+}
+
+// H指数
+func hIndex(citations []int) int {
+	n := len(citations)
+	sort.Ints(citations)
+	ans := 0
+	for i := n - 1; i >= 0; i-- {
+		if citations[i] >= n-i {
+			ans = n - i
+		}
+	}
+	return ans
 }
